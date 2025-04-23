2b5d3cd4ef300275b913dc35048749a67b3a1d0c
JIAtype <148043337+JIAtype@users.noreply.github.com>
2025-04-23 20:48:12 +0800

    Update business.py

--- app/pages/business.py
+++ app/pages/business.py
@@ -20,13 +20,13 @@ def show_business_page():
     # 分析类型选择
     analysis_type = st.sidebar.radio(
         "Select Analysis",
-        ["Video Summary","Sentiment Analysis","Topic Analysis","Entity Analysis", "VADER Sentiment Analysis","ROBERTA ABSA Sentiment Analysis","TD-IDF VADER Sentiment Analysis","TD-IDF ROBERTA ABSA Sentiment Analysis","TD-IDF BART ABSA Sentiment Analysis","Analysis Comparison"]
+        ["Video Summary","Sentiment Summary","Topic Analysis","Entity Analysis", "VADER Sentiment Analysis","ROBERTA ABSA Sentiment Analysis","TD-IDF VADER Sentiment Analysis","TD-IDF ROBERTA ABSA Sentiment Analysis","TD-IDF BART ABSA Sentiment Analysis","Analysis Comparison"]
     )
     
     # 显示选定的分析
     if analysis_type == "Analysis Comparison":
         show_creator_comparison()
-    elif analysis_type == "Sentiment Analysis":
+    elif analysis_type == "Sentiment Summary":
         show_sentiment_summary()
     elif analysis_type == "Video Summary":
         show_video_summary()
