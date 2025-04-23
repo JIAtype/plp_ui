81ff09aef3115ccdc71a718aa8072efef50a33ac
JIAtype <148043337+JIAtype@users.noreply.github.com>
2025-04-24 07:50:54 +0800

    Update tdidf_roberta_sentiment_analysis.py

--- app/components/tdidf_roberta_sentiment_analysis.py
+++ app/components/tdidf_roberta_sentiment_analysis.py
@@ -78,7 +78,7 @@ def show_tdidf_roberta_sentiment_analysis():
     with col2:
         st.header("ROBERTA ABSA Sentiment Analysis")
         st.write("TD-IDF Extracted Topics As Aspects")
-    st.markdown("---")
+    # st.markdown("---")
     
     # Add progress indicator for initial data loading
     with st.spinner('Loading sentiment data...'):
