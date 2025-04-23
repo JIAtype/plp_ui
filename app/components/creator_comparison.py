e7bf29a3f908b8ce9f0d49035fd60094ef891722
JIAtype <148043337+JIAtype@users.noreply.github.com>
2025-04-23 16:57:59 +0800

    Update creator_comparison.py

--- app/components/creator_comparison.py
+++ app/components/creator_comparison.py
@@ -22,7 +22,7 @@ def show_creator_comparison():
         analysis_type_1 = st.selectbox(
             "Select First Analysis Type",
             options=analysis_types,
-            index=4,
+            index=0,
             key="analysis_1"
         )
     
@@ -31,7 +31,7 @@ def show_creator_comparison():
         analysis_type_2 = st.selectbox(
             "Select Second Analysis Type",
             options=analysis_types,
-            index=5, 
+            index=0, 
             key="analysis_2"
         )
     
